a3822a20fddfadb0150805124d2639f87bf72024
jeongdari <dannyjeong.au@gmail.com>
2024-05-15 11:26:31 +1000

    After a debugging

--- obj/Debug/net8.0/LibraryManagementSystem.AssemblyInfo.cs
+++ obj/Debug/net8.0/LibraryManagementSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10876c5f59fc3081b478d8f79061fec7e915cf7a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c085cb233aacca7360b963a7cee4c7a904439660")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
